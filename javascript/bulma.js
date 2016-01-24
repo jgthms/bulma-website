62a51347aee8f094b5680783a9381022f7b854a2
jgthms <bbxdesign@gmail.com>
2016-01-24 01:39:55 +0000

    Init

---
+++ javascript/bulma.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function ($) {
+
+  var $toggle = $('#header-toggle');
+  var $menu = $('#header-menu');
+
+  $toggle.click(function() {
+    $(this).toggleClass('is-active');
+    $menu.toggleClass('is-active');
+  });
+
+});
